c15fda328c506b1255e8070ed12ae317ab202ff6
Yeampier2022 <yeampierjesusa@gmail.com>
2023-02-14 13:38:49 -0400

    madejo de datos

---
+++ tiposDatos.py
@@ -0,0 +1,15 @@
+#Tipo entero
+x = 10
+print(type(x))
+# Tipo float 
+x = 14.5
+print(type(x))
+# Tipo string 
+x = 'Hola python'
+print(type(x))
+# Tipo bool 
+x = True
+print(type(x))
+# Tipo bool 
+x = False
+print(type(x))
--- variable.py
+++ variable.py
@@ -1,2 +1,24 @@
-Mivariable = "hola estoy aprediendo python"
-print( Mivariable )
+Mivariable = True
+print(Mivariable)
+Mivariable = 1
+print(Mivariable)
+Mivariable = "Curso en udemy"
+print(Mivariable)
+
+
+x = 10
+y = 12
+z = x + y
+print(x)
+print(y)
+print(z)
+
+nombre = "Yeampier Huerta"
+telefono =  412188975
+correo = "Yeampierjesusa@gmail.com"
+
+print(nombre)
+print(telefono)
+print(correo)
+
+
